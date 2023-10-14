66305b03898b7edbee05d094e4d34adb93dfa56a
Dan <dan.s.boltachev@yandex.ru>
2023-10-15 01:57:34 +0300

    Третий commit

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class DepositCalculator {
     private final double YEAR_RATE = 0.06;
-    private final int EXPONENT = 2;
-    double calculateWithCapitalization(int amount, double yearRate, int depositPeriod) {
-        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
-        return round(pay, 2);
+
+    double calculateWithCapitalization(int amount, int depositPeriod) {
+        double pay = amount * Math.pow((1 + YEAR_RATE / 12), 12 * depositPeriod);
+
+        return round(pay);
     }
 
-    double calculateWithoutCapitalization(double amount, double yearRate, int depositPeriod) {
-        return round(amount + amount * yearRate * depositPeriod, EXPONENT);
+    double calculateWithoutCapitalization(double amount, int depositPeriod) {
+        return round(amount + amount * YEAR_RATE * depositPeriod);
     }
 
-    double round(double value, int exponent) {
-        double scale = Math.pow(10, exponent);
+    double round(double value) {
+        int EXPONENT = 2;
+        double scale = Math.pow(10, EXPONENT);
+
         return Math.round(value * scale) / scale;
     }
 
@@ -26,13 +29,18 @@ public class DepositCalculator {
         int amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
         depositPeriod = scanner.nextInt();
+
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
+
         double finResult = 0;
-        if (action == 1) finResult = calculateWithoutCapitalization(amount, YEAR_RATE, depositPeriod);
-        else if (action == 2) {
-            finResult = calculateWithCapitalization(amount, YEAR_RATE, depositPeriod);
+
+        if (action == 1) {
+            finResult = calculateWithoutCapitalization(amount, depositPeriod);
+        } else if (action == 2) {
+            finResult = calculateWithCapitalization(amount, depositPeriod);
         }
+
         System.out.println("Результат вклада: " + amount + " за " + depositPeriod + " лет превратятся в " + finResult);
     }
 
